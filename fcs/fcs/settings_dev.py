eea23bb8ddf1516f759ab872ddb810d0ec3058c2
Krzysztof Dorosz <cypreess@gmail.com>
2013-11-24 17:08:02 +0100

    overriding default db settings for dev env

--- fcs/fcs/settings_dev.py
+++ fcs/fcs/settings_dev.py
@@ -1,2 +1,14 @@
 #noinspection PyUnresolvedReferences
 from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fcs',
+        'USER': 'fcs',
+        'PASSWORD': 'fcs',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+}
+
