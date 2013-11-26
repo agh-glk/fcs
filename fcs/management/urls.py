7daef5fcfe3a38ed59b2863639db31d87888d7a6
Gabor <pergarbor@gmail.com>
2013-11-26 16:43:23 +0100

    Base log-in mechanism (without registering users)

--- fcs/management/urls.py
+++ fcs/management/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.login, name='login'),
 )
