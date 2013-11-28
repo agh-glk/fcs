e23c27f665414187233342ff9fc1988b71515039
glegien <glegien@gmail.com>
2013-11-28 15:22:53 +0100

    Changed model. Admin configured. ChangePassword function added.

--- fcs/management/urls.py
+++ fcs/management/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^reg_scs', views.registration_successful),
     url(r'^login', views.login_user, name='login'),
     url(r'^logout',views.logout_user, name='logout'),
+    url(r'^change_password', views.change_password, name='change_password')
 
 )
