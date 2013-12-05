f61cd7e4fe1d429571dfe3df2740d5541b49aa28
Gabor <pergarbor@gmail.com>
2013-12-05 14:44:04 +0100

    Improved login and registration page layouts

---
+++ fcs/fcs/manager/static/js/registration-form.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#id_username').addClass('form-control');
+  $('#id_username').attr('placeholder','Username');
+  $('#id_email').addClass('form-control');
+  $('#id_email').attr('placeholder','Email');
+  $('#id_password1').addClass('form-control');
+  $('#id_password1').attr('placeholder','Password');
+  $('#id_password2').addClass('form-control');
+  $('#id_password2').attr('placeholder','Confirm password');
+});
