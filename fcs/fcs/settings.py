9abed427f876978dc8a165dfc0550ab8e5d31539
glegien <glegien@gmail.com>
2014-01-21 13:53:36 +0100

    [FCS-11.1]Huey and Redis vagrant conf 2.

--- fcs/fcs/settings.py
+++ fcs/fcs/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 HUEY = {
     'backend': 'huey.backends.redis_backend',  # required.
     'name': 'unique name',
-    'connection': {'host': 'localhost', 'port': 7897},
+    'connection': {'host': 'localhost', 'port': 6379},
     'always_eager': False, # Defaults to False when running via manage.py run_huey
 
     # Options to pass into the consumer when running ``manage.py run_huey``
