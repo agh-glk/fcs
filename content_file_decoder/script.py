dabc5a356c32ce15e071464c6e01843f00706a14
glegien <glegien@gmail.com>
2014-04-29 12:30:51 +0200

    [FCS-39] Script.

---
+++ content_file_decoder/script.py
@@ -0,0 +1,38 @@
+import sys
+import json
+import os
+import shutil
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        print 'Required: python script.py <file> <directory>'
+        exit(1)
+    file_path = sys.argv[1]
+    dest_path = os.path.abspath(sys.argv[2])
+    dir_path = os.path.join(dest_path, str(os.path.split(file_path)[1]))
+    shutil.rmtree(dir_path, ignore_errors=True)
+    os.mkdir(dir_path)
+    counter = 1
+    with open(file_path) as data_file:
+        for line in data_file:
+            json_object = json.loads(line)
+            inner_dir_path = os.path.join(dir_path, str(counter))
+            os.mkdir(inner_dir_path)
+            with open(os.path.join(inner_dir_path, 'url_links.txt'), 'w+') as url_links:
+                url_links.write(json_object['url']+'\n')
+                url_links.write(" ".join(json_object['links']))
+            with open(os.path.join(inner_dir_path, 'content.dat'), 'w+') as content_file:
+                content_file.write(json_object['content'].decode('base64'))
+            counter += 1
+    exit(0)
+
+
+
+
+
+
+
+
+
+
