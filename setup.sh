41014540748ccef1d1084794dd1fc2bd0f3fabc2
glegien <glegien@gmail.com>
2014-05-26 11:20:27 +0200

    [NT]  Berkeley & bsddb3 on vagrant.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
 tar -xzf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix
 sudo ../dist/configure --enable-cxx
-make
-make install
+sudo make
+sudo make install
