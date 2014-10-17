b8b436efd19a73b0e970371c7d99d9252c4202a8
glegien <glegien@gmail.com>
2014-10-17 14:48:53 +0200

    [FCS-39] Crawling results decoder.

--- content_file_decoder/script.py
+++ content_file_decoder/script.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
         exit(1)
     file_path = sys.argv[1]
     dest_path = os.path.abspath(sys.argv[2])
-    dir_path = os.path.join(dest_path, str(os.path.split(file_path)[1]))
+    dir_path = os.path.join(dest_path, str(os.path.splitext(file_path)[0]))
     shutil.rmtree(dir_path, ignore_errors=True)
     os.mkdir(dir_path)
     counter = 1
+    index = []
     with open(file_path) as data_file:
         for line in data_file:
             json_object = json.loads(line)
@@ -22,9 +23,15 @@ if __name__ == '__main__':
             with open(os.path.join(inner_dir_path, 'url_links.txt'), 'w+') as url_links:
                 url_links.write(json_object['url']+'\n')
                 url_links.write(" ".join(json_object['links']))
+                index.append(json_object['url'])
             with open(os.path.join(inner_dir_path, 'content.dat'), 'w+') as content_file:
                 content_file.write(json_object['content'].decode('base64'))
             counter += 1
+        with open(os.path.join(dir_path, 'index.txt'), 'w+') as index_file:
+            iterator = 1
+            for entry in index:
+                index_file.write('%s : %s\n' % (iterator, entry))
+                iterator += 1
     exit(0)
 
 
